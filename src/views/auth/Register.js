4505fa614466f0557f3339e36e22be7059dd8091
Jake Hoffman <jaketh19@gmail.com>
2022-07-29 16:53:17 -0400

    Made register UI look nicer

--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -1,9 +1,11 @@
 import React, { useState, useContext } from "react";
 import { Navigate } from "react-router-dom";
 import firebaseConfig from "../../config";
+import firebase from "firebase/compat/app";
 
 import { AuthContext } from "../../components/auth/AuthProvider";
 
+export const auth = firebaseConfig.auth();
 export default function Register() {
 
   // const [currentUser, setCurrentUser] = useState(null);
@@ -21,50 +23,63 @@ export default function Register() {
     }
   };
 
+  const handleGoogleLogin = () => {
+    try {
+      const googleProvider = new firebase.auth.GoogleAuthProvider();
+      auth.signInWithPopup(googleProvider);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   if (currentUser) {
-    console.log('test')
     return <Navigate to="/words" />
-  } else {
-    console.log('not test')
   }
 
   return (
-    <>
-      <button>Google</button>
-      <small>Or with email</small>
+    <div className="w-full h-full flex flex-col justify-center content-center items-center">
+      <div className="w-fit flex flex-row items-center mb-4">
+        <h1>Sign in with</h1>
+        <button type="button" onClick={ () => handleGoogleLogin() }
+          className="ml-2 p-2 border-2 text-center rounded">Google</button>
+      </div>
+      <small>Or create an account with email</small>
       <form onSubmit={ handleSubmit }>
-        <div>
-          <label htmlFor="grid-password">
+        <div className="flex flex-row m-2">
+          <label htmlFor="grid-password" className="basis-1/3">
             Name 
           </label>
-          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Name" 
+            className="text-black text-center grow ml-2"/>
         </div>
-        <div>
-          <label htmlFor="grid-password">
+        <div className="flex flex-row m-2">
+          <label htmlFor="grid-password" className="basis-1/3">
             Email
           </label>
-          <input type="email" placeholder="Email" name="email"/>
+          <input type="email" placeholder="Email" name="email"
+            className="text-black text-center grow ml-2"/>
         </div>
-        <div>
-          <label htmlFor="grid-password">
-            password
+        <div className="flex flex-row m-2">
+          <label htmlFor="grid-password" className="basis-1/3">
+            Password
           </label>
-          <input type="password" placeholder="Password" name="password"/>
+          <input type="password" placeholder="Password" name="password"
+            className="text-black text-center grow ml-2"/>
         </div>
         <div>
-          <label>
+          {/* <label>
             <input type="checkbox" />
             <span>I agree with the{" "} 
               <a href="#temp">Privacy Policy</a>
             </span>
-          </label>
+          </label> */}
         </div>
-        <div>
+        <div className="text-center border-2 rounded m-4">
           <button type="submit">
             Create Account
           </button>
         </div>
       </form>
-    </>
+    </div>
   );
 }
