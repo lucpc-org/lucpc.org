0d9c4a8a226da8d7893f01202856b07e624f04d8
Kyle Wells <47221196+kqwq@users.noreply.github.com>
2023-06-16 18:56:46 -0400

    Fix top navigation colors

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,13 +1,15 @@
-"use client"
+"use client";
 
 import React, { useContext } from "react";
 import Link from "next/link";
 import { AuthContext } from "./AuthProvider";
-import RedLink from "./RedLink";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
   const { currentUser } = useContext(AuthContext);
-  
+
+  const currentPath = usePathname();
+
   const navLinks = [
     {
       name: "About",
@@ -21,27 +23,41 @@ export default function NavBar() {
       name: "Competition",
       path: "/competition",
     },
+    ...(currentUser
+      ? [
+          {
+            name: "Profile",
+            path: "/profile",
+          },
+        ]
+      : [
+          {
+            name: "Log In",
+            path: "/auth/login",
+          },
+        ]),
   ];
   return (
     <div className="w-full flex justify-center pb-12 md:pb-[6rem] sm:text-base md:text-lg">
       <div className="w-[95%] md:w-[90%] md:h-[70px] mt-2 flex flex-col md:flex-row justify-center md:justify-between items-center">
         <Link href="/" className="flex pb-2">
-          <p className="flex justify-center font-bold text-2xl md:text-3xl btn btn-ghost">🧑‍💻 CPC</p>
+          <p className="flex justify-center font-bold text-2xl md:text-3xl btn btn-ghost">
+            🧑‍💻 CPC
+          </p>
         </Link>
 
         <div className="flex flex-row items-center justify-center text-sm md:text-base lg:text-lg xl:text-xl xl:space-x-10 space-x-5 lg:space-x-7">
-          {navLinks.map(({ name, path }) => (
-            <Link href={path} key={name}>
-              <div className="text-text_color underline decoration-dotted decoration-background hover:decoration-gray-400 hover:text-text_hover transition-all duration-150">
-                {name}
-              </div>
-            </Link>
-          ))}
-          {currentUser ?
-            <RedLink to="/profile" label="Profile" extraStyles="text-green decoration-2"/>
-           : 
-            <RedLink to="/auth/login" label="Log In" extraStyles="text-green decoration-2"/>
-          }
+          {navLinks.map(({ name, path }) => {
+            const extraStyles =
+              currentPath === path
+                ? "text-green decoration-2 font-bold underline cursor-default"
+                : "text-text_color underline decoration-dotted decoration-background hover:decoration-gray-400 hover:text-text_hover transition-all duration-150";
+            return (
+              <Link href={path} key={name}>
+                <div className={`${extraStyles} `}>{name}</div>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </div>
