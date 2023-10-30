b70415a927a6703e36000cd138cbdde15453ef81
cameron <me@cameron.rs>
2023-10-30 11:42:29 -0400

    update metadata

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,6 +2,14 @@ export default function Head() {
   return <>
     <title>LUCPC</title>
     <meta name="description" content="A club to teach core programming concepts concepts along with preparing students to compete at the International Collegiate Programming Contest. We welcome all skill levels." />
-
+    <meta name="og:title" content="Liberty University's Competitive Programming Club" />
+    <meta name="og:description" content="A club to teach core programming concepts concepts along with preparing students to compete at the International Collegiate Programming Contest. We welcome all skill levels." />
+    <meta name="og:type" content="website" />
+    <meta name="og:url" content="https://lucpc.org" />
+    <meta name="og:site_name" content="lucpc.org"/>
+    <meta name="twitter:title" content="Liberty University's Competitive Programming Club" />
+    <meta name="twitter:description" content="A club to teach core programming concepts concepts along with preparing students to compete at the International Collegiate Programming Contest. We welcome all skill levels." />
+    <meta name="theme-color" content="#0A254E" />
+    
   </>
 }
