3ea0af409ac6b3900d1032f287cdb6afb552f5fc
cameron <me@cameron.rs>
2024-02-05 14:17:07 -0500

    officer interest form

---
+++ src/app/officer-interest/page.js
@@ -0,0 +1,19 @@
+"use client";
+
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+const LINK = "https://forms.gle/77zUoRKxpYEQR9is8";
+
+export default function DiscordRedirect() {
+    redirect(LINK);
+
+  useEffect(() => {
+    window.location.href = LINK;
+  }, []);
+
+  return (
+    <a href={LINK}>
+      Redirecting... if you are not automatically redirected, click <b className="text-blue">here</b>
+    </a>
+  );
+}
